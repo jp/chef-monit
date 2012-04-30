dc01fae1144ad162de5413e91cde10065a1ae024
Brad Montgomery <brad@bradmontgomery.net>
2012-04-30 11:42:20 -0500

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "bmontgomery@coroutine.com"
 license          "MIT"
 description      "Configures monit.  Originally based off the 37 Signals Cookbook."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.8.2"
+version          "0.8.3"
 
 recipe           "monit::default", "install/configure monit"
 recipe           "monit::services", "configure services for monit to monitor"
