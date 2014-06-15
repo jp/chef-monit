0118df381bc4ee21698d6a2264978492815dda57
Julien Pellet <pelletj@gmail.com>
2014-06-15 18:21:27 +0200

    add name to cookbook for Berkshelf

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ license          "MIT"
 description      "Configures monit.  Originally based off the 37 Signals Cookbook."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.8.5"
+name "monit"
 
 recipe           "monit::default", "install/configure monit"
 recipe           "monit::services", "configure services for monit to monitor"
